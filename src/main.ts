ac61c1186af003027aeabd4971c8653436a1c686
Bloomy <contact@bloomyindev.me>
2025-11-10 23:18:47 +0100

    Remove dynamic imports (use static one instead)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+// Base imports
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 import './styles/main.css'
+
+// Views
+import HomeView from '@views/HomeView.vue'
+import NotFoundView from '@views/NotFoundView.vue'
 
 export const createApp = ViteSSG(
     App,
@@ -8,11 +13,11 @@ export const createApp = ViteSSG(
         routes: [
             {
                 path: '/',
-                component: await import('@views/HomeView.vue'),
+                component: HomeView,
             },
             {
                 path: '/:pathMatch(.*)*',
-                component: await import('@views/NotFoundView.vue'),
+                component: NotFoundView,
             },
         ],
     },
