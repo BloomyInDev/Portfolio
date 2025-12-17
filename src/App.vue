432bf73a45a0cce7a94c31d032b96c43d0f0a22c
Bloomy <contact@bloomyindev.me>
2025-12-17 21:25:45 +0100

    feat: enhance App.vue with transition effects and dynamic RouterView rendering

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
-import FooterComponent from '@components/layout/FooterComponent.vue'
-import HeaderComponent from '@components/layout/HeaderComponent.vue'
+import FooterComponent from "@components/layout/FooterComponent.vue"
+import HeaderComponent from "@components/layout/HeaderComponent.vue"
+import { RouterView, useRoute } from "vue-router"
+
+const route = useRoute()
 </script>
 
 <template>
     <HeaderComponent />
-    <main><RouterView /></main>
+    <main>
+        <RouterView v-slot="{ Component }">
+            <Transition name="fade" mode="out-in">
+                <component :is="Component" :key="route.path" />
+            </Transition>
+        </RouterView>
+    </main>
     <FooterComponent />
 </template>
 
-<style scoped></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
