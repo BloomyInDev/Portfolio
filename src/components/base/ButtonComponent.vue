48ea3582c7ad50f5090c482e3a43e3735bd62cb0
Bloomy <contact@bloomyindev.me>
2025-12-17 15:59:44 +0100

    style: update ButtonComponent styles for improved appearance and transition effects

--- src/components/base/ButtonComponent.vue
+++ src/components/base/ButtonComponent.vue
@@ -8,15 +8,16 @@
 .btn {
     padding: 0.5rem 1rem;
     border: none;
-    border-radius: 0.25rem;
+    border-radius: 0.75rem;
     background-color: #268fff;
     color: #ffffff;
     font-size: 1rem;
     cursor: pointer;
-    transition: background-color 250ms ease-in-out;
+    transition: all 250ms ease-in-out;
 }
 .btn:hover {
     background-color: #0069da;
+    border-radius: 1rem;
 }
 
 .btn > * {
