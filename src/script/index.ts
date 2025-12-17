5e798fd4cb1085d2e13bff126bbc74fbb647b588
Bloomy <contact@bloomyindev.me>
2025-12-17 16:00:36 +0100

    feat: add TechnologiesEnum and MethodsEnum enums for technology categorization

---
+++ src/script/index.ts
@@ -0,0 +1,28 @@
+export enum TechnologiesEnum {
+    // Web
+    HTML = "html",
+    CSS = "css",
+    JAVASCRIPT = "js",
+    TYPESCRIPT = "ts",
+    PHP = "php",
+    VUEJS = "vue.js",
+
+    // Langages haut niveau
+    JAVA = "java",
+    PYTHON = "python",
+    GOLANG = "go",
+
+    // Langages bas niveau
+    C = "c",
+
+    // Langages de base de données
+    SQL = "sql",
+    PLSQL = "plsql",
+
+    // Coordination
+    GIT = "git",
+}
+
+export enum MethodsEnum {
+    AGILE = "agile",
+}
