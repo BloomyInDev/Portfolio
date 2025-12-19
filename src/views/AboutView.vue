8cf624c9d272bc2ecc322d86b1e631aca5101e18
Bloomy <contact@bloomyindev.me>
2025-12-19 23:14:50 +0100

    feat: restructure AboutView with improved layout and contact information

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-    <p>A propos</p>
+    <section>
+        <h2>A propos</h2>
+        <p>
+            Pour me contacter, n'hésitez pas à m'envoyer un email à l'adresse
+            <a href="mailto:contact+portfolio@bastienluben.dev"
+                >contact+portfolio@bastienluben.dev</a
+            >
+        </p>
+        <p>Vous pouvez également obtenir un lien temporaire pour mon portfolio ici.</p>
+    </section>
 </template>
+
+<style scoped>
+section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+a {
+    color: #ffffff;
+}
+</style>
