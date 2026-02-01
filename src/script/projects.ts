3851a58f9579c17b6ade479c939bc4a567c24308
Bloomy <contact@bloomyindev.me>
2026-02-01 23:09:39 +0100

    feat: updated project "CodeGameJam 2026"

--- src/script/projects.ts
+++ src/script/projects.ts
@@ -256,7 +256,7 @@ const unsortedProjects: IIncompleteProject[] = [
         description: [
             "Lors de la 10ème édition de la CodeGameJam, j'ai de nouveau eu l'opportunité de participer à cet événement passionnant où j'ai travaillé en équipe pour développer un jeu vidéo en 30 heures.",
             'Le thème de cette édition étant "Faites des clics", nous avons décidé d\'utiliser à fond le concept des clics.',
-            'Notre jeu, intitulé "Le carnaval de Makoto", est un jeu infini, où le joueur passe à travers des niveaux de catégories aléatoires et de difficultés croissantes.',
+            'Notre jeu, intitulé "Le carnaval de Makoto", est un jeu infini où le joueur passe à travers des niveaux de catégories aléatoires et de difficultés croissantes.',
             "Dans un niveau, il faudra cliquer sur les bombes avant qu'elles n'explosent.",
             "Dans un autre, il faudra cliquer sur les ennemis qui viendront vers nous avant qu'elles ne nous touchent.",
             "Ce projet m'a permis de renforcer mes compétences en développement de jeux vidéo, en gestion du temps et en travail d'équipe.",
@@ -283,7 +283,14 @@ const unsortedProjects: IIncompleteProject[] = [
             start: createDate({ day: 15, month: MonthEnum.January, year: 2026 }),
             end: createDate({ day: 17, month: MonthEnum.January, year: 2026 }),
         },
-        images: [],
+        images: [
+            { title: "Menu principal", url: "/project/cgj2026/homescreen.png" },
+            { title: "Écran de jeu 1", url: "/project/cgj2026/gameplay_1.png" },
+            { title: "Écran de jeu 2", url: "/project/cgj2026/gameplay_2.png" },
+            { title: "Écran de jeu 3", url: "/project/cgj2026/gameplay_3.png" },
+            { title: "Écran de jeu quand on meurt", url: "/project/cgj2026/gameplay_dead.png" },
+            { title: "Écran des crédits", url: "/project/cgj2026/credits.png" },
+        ],
         projectUrl: "https://bybloomy.itch.io/le-carnaval-de-makoto",
     },
 ]
