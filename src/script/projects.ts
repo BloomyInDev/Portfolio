6f81316886d4dfb23e233e24bad93a9202d413f5
Bloomy <contact@bloomyindev.me>
2026-04-15 15:08:56 +0200

    feat: add new project entry for internship at Dmoti

--- src/script/projects.ts
+++ src/script/projects.ts
@@ -432,6 +432,16 @@ const unsortedProjects: IIncompleteProject[] = [
         },
         knowledges: [TechnologiesEnum.LINUX, TechnologiesEnum.DOCKER, MethodsEnum.SELF_LEARNING],
     },
+    {
+        title: "Stage chez Dmoti",
+        subtitle:
+            "Stage de développement web chez Dmoti, une entreprise spécialisée dans la Gestion Technique des Batiments (GTB)",
+        description: ["WIP"],
+        dates: { start: createDate({ month: MonthEnum.April, year: 2026 }) },
+        knowledges: [],
+        newFormat: true,
+        type: ProjectTypeEnum.PROFESSIONAL,
+    },
 ]
 
 export const projects = unsortedProjects
